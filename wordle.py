ecdd679994c5adf1432310b21c40b09c2a86fcbb
Richard Duarte <richard.c.duarte@gmail.com>
2022-01-26 10:06:53 -0800

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -1,8 +1,8 @@
 import click, re
 
 @click.command()
-@click.option('--contains', default="", help='Letters that must be included. Example: "olk"')
-@click.option('--indices', default="", help='Comma separated list of letters and indices.  Example: "f0,o1,l2,k3,s4"')
+@click.option('--contains', default="", help='Comma separated list of letters of indices.  Indicates that these letters must be included NOT at these indices. Example: "o0,l1,k2"')
+@click.option('--indices', default="", help='Comma separated list of letters and indices.  Indicates that these letters MUST be included at these indices. Example: "f0,o1,l2,k3,s4"')
 @click.option('--impossible', default="", help='Letters that must be excluded.  Example: "abcdeghijmnpqrtuvwxyz"')
 @click.option('--inputfile', default="5letterwords.txt", help='Word list to use. Default: "5letterwords.txt"')
 def solve(contains, indices, impossible, inputfile):
@@ -11,6 +11,7 @@ def solve(contains, indices, impossible, inputfile):
     with open(inputfile) as file:
         fiveletterwords = file.readlines()
     indices = indices.split(",")
+    contains = contains.split(",")
 
     for word in fiveletterwords:
         word = word.strip()
@@ -22,9 +23,14 @@ def solve(contains, indices, impossible, inputfile):
                     break
         if correct and len(contains) > 0:
             for letter in contains:
-                if letter not in word:
+                if letter[0] not in word:
                     correct = False
                     break
+                elif letter and word[int(letter[1])] == letter[0]:
+                    correct = False
+                    break
+                else:
+                    print 
         if correct and len(impossible) > 0:
             for letter in impossible:
                 if letter in word:
